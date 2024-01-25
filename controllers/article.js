abb8476837c989777e53f32b87c734d15012673e
Ayrton Mägipõld <ayrtonm0@gmail.com>
2024-01-26 01:46:18 +0200

    “Getted article data by slug”

--- controllers/article.js
+++ controllers/article.js
@@ -9,17 +9,32 @@ const Article = require('../models/article')(sequelize, Sequelize.DataTypes)
 
 const getAllArticles = (req, res) => {
     const articles = Article.findAll()
-    .then(articles => {
-        console.log(articles)
-        return res.status(200).json({articles})
+        .then(articles => {
+            console.log(articles)
+            return res.status(200).json({ articles })
+        })
+        .catch(error => {
+            return res.status(500).send(error.message)
+        })
+
+}
+
+const getArticleBySlug = (req, res) => {
+    Article.findOne({
+        where: {
+            slug: req.params.slug
+        }
     })
-    .catch( error =>{
-        return res.status(500).send(error.message)
-    })
-    
+        .then(article => {
+            console.log(article)
+            return res.status(200).json({ article })
+        })
+        .catch(error => {
+            return res.status(500).send(error.message)
+        })
 }
 
 module.exports = {
     getAllArticles,
-
+    getArticleBySlug
 }
--- index.js
+++ index.js
@@ -4,23 +4,24 @@ require('dotenv').config()
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
-  })
+})
 
-sequelize.authenticate().then(()=>{console.log('Connected to the database')}).catch(err =>{
+sequelize.authenticate().then(() => { console.log('Connected to the database') }).catch(err => {
     console.error('Unable to connect to the database: ', err)
 })
 
 
 
 const articleRouter = require('./routes/article')
-app.use('/',articleRouter)
+app.use('/', articleRouter)
+app.use('/article', articleRouter)
 
-app.listen(port, ()=>{
-    console.log('Server is running on localhost:'+ port)
+app.listen(port, () => {
+    console.log('Server is running on localhost:' + port)
 })
