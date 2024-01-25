f98a03bbff0bc54d29dfae543d2dd1e6eb06b46f
Ayrton Mägipõld <ayrtonm0@gmail.com>
2024-01-26 00:50:52 +0200

    “Created connection to database”

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  })
+
+sequelize.authenticate().then(()=>{console.log('Connected to the database')}).catch(err =>{
+    console.error('Unable to connect to the database: ', err)
+})
+
+
 
 app.get('/',(req,res)=> {
     res.json({message: 'Welcome to sequelize application.'})
